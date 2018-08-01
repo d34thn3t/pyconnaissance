56f5b16f113823f8b4aaa282a2fd888e2e02814d
d34thn3t <safiy.malik@gmail.com>
2018-08-01 10:38:57 -0700

    Update pyconnaissance.py

--- pyconnaissance.py
+++ pyconnaissance.py
@@ -5,17 +5,23 @@ import sys
 import os
 import subprocess
 
+#make sure all arguments are provided, if not then exit with this message
+
 if len(sys.argv)!=3:
 
 	print("Usage: python3 pyconnaissance.py <interface> <log file>")
 
 	exit()
+	
+#get host address on current interface
 
 def get_local_addr():
 
 	local_addr=nf.ifaddresses(sys.argv[1])[AF_INET][0]['addr']
 
 	return local_addr
+
+#get range of ip addresses to be used by nmap
 
 def get_ip_range():
 
@@ -45,6 +51,8 @@ def get_ip_range():
 
 	return addr
 
+#get the router ip address
+
 def get_default_gateway():
 
 	t=0
@@ -73,31 +81,55 @@ def get_default_gateway():
 
 	return addr
 
+#use nmap to scan all hosts on the network
+
 def netmap(range_ip):
 
 	os.system("nmap -T4 -O -sV -oX - "+range_ip+" > "+sys.argv[2]+".xml")
+	
+#convert nmap's xml output to html
 
 def xml_to_html():
 
 	os.system("xsltproc "+sys.argv[2]+".xml > "+sys.argv[2]+".html")
 	
 	os.system("rm "+sys.argv[2]+".xml")
+	
+#main code
+
+#get start time
 
 start_time = datetime.datetime.now()
 
+#create log file 
+
 f=open(sys.argv[2], "w+")
+
+#write time into log file
 
 f.write("Started at "+str(start_time)+"\n")
 
+#write host address into log file
+
 f.write("Host Address: "+get_local_addr()+"\n")
+
+#set value to the ip range for nmap
 
 ip_range=get_ip_range()
 
+#set value to router ip
+
 default_gateway=get_default_gateway()
+
+#close main log
 
 f.close()
 
+#run nmap
+
 netmap(ip_range)
+
+#conver xml output of nmap to html
 
 xml_to_html()
 
